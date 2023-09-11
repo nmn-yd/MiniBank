1541699fdf91ffc484c6c67260a7a60b05d8d6ab
Naman Yadav <raymannky2001@gmail.com>
2023-09-12 00:01:08 +0530

    final commit

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -15,76 +15,44 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan > 0) return state;
 
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance += Number(action.payload.amount);
+    requestLoan: {
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return state;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += Number(action.payload.amount);
+      },
     },
 
-    payLoan(state, action) {
+    payLoan(state) {
+      state.balance = state.balance - state.loan;
       state.loan = 0;
       state.loanPurpose = "";
-      state.balance = state.balance - state.loan;
+    },
+    currencyGettingConverted(state) {
+      state.isLoading = true;
     },
   },
 });
 
-console.log(accountSlice);
-
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 
 export default accountSlice.reducer;
-
-/*
-export default function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload,
-        isLoading: false,
-      };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + Number(action.payload.amount),
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    case "account/currencyGettingConverted":
-      return {
-        ...state,
-        isLoading: true,
-      };
-
-    default:
-      return state;
-  }
-}
-
-// Account actions functions
 
 export function deposit(amount, currency) {
   //if currency is dollar then return action
   if (currency === "USD") return { type: "account/deposit", payload: amount };
-
-  console.log(amount, currency);
 
   //if not then lets use redux thunk to convert it into USD  using a API call.
   return async function (dispatch, getState) {
@@ -103,16 +71,3 @@ export function deposit(amount, currency) {
     dispatch({ type: "account/deposit", payload: converted });
   };
 }
-
-export function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-export function requestLoan(amount, purpose) {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-}
-
-export function payLoan() {
-  return { type: "account/payLoan" };
-}
-*/
--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,43 +1,81 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 //Customer
-const initialStateCustomer = {
+
+const initialState = {
   fullName: "",
   nationalId: "",
   createdAt: "",
 };
 
-export default function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalId: action.payload.nationalId,
-        createdAt: action.payload.createdAt,
-      };
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
 
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload,
-      };
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalId) {
+        return {
+          payload: {
+            fullName,
+            nationalId,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
 
-    default:
-      return state;
-  }
-}
+      reducer(state, action) {
+        state.fullName = action.fullName;
+        state.nationalId = action.payload.nationalId;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
 
-// customer actions function
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
 
-export function createCustomer(fullName, nationalId) {
-  return {
-    type: "customer/createCustomer",
-    payload: { fullName, nationalId, createdAt: new Date().toDateString() },
-  };
-}
+export const { createCustomer, updateName } = customerSlice.actions;
 
-export function updateName(fullName) {
-  return {
-    type: "customer/updateName",
-    payload: fullName,
-  };
-}
+export default customerSlice.reducer;
+
+// old way
+
+// export default function customerReducer(state = initialStateCustomer, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalId: action.payload.nationalId,
+//         createdAt: action.payload.createdAt,
+//       };
+
+//     case "customer/updateName":
+//       return {
+//         ...state,
+//         fullName: action.payload,
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
+
+// // customer actions function
+
+// export function createCustomer(fullName, nationalId) {
+//   return {
+//     type: "customer/createCustomer",
+//     payload: { fullName, nationalId, createdAt: new Date().toDateString() },
+//   };
+// }
+
+// export function updateName(fullName) {
+//   return {
+//     type: "customer/updateName",
+//     payload: fullName,
+//   };
+// }
